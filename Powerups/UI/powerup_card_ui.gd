b6d41bd5fc892d6996dda40a4a36c7cb77e0b880
gilzoide <gilzoide@gmail.com>
2024-01-27 11:51:34 -0300

    Update buy powerup button when points change

--- Powerups/UI/powerup_card_ui.gd
+++ Powerups/UI/powerup_card_ui.gd
@@ -4,11 +4,19 @@ extends Control
 @export var score: ScoreResource
 @export var powerup: PowerUp
 
+@onready var buy_button: Button = $BuyGroup/BuyButton
+
 
 func _ready():
 	$Texts/Title.text = powerup.title
 	$Texts/Description.text = powerup.description
 	$BuyGroup/Texts/Value.text = str(powerup.cost)
+	score.on_points_changed.connect(_on_points_changed)
+	_on_points_changed(score.points)
+
+
+func _on_points_changed(points):
+	buy_button.disabled = not score.can_spend(powerup.cost)
 
 
 func _on_buy_button_pressed():
