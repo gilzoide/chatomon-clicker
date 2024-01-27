99a1b679c8a216c209a7b891da0c9f4051b08ed9
gilzoide <gilzoide@gmail.com>
2024-01-27 10:19:07 -0300

    Add `on_points_changed` event to ScoreResource

--- Score/score_resource.gd
+++ Score/score_resource.gd
@@ -1,5 +1,7 @@
 extends Resource
+class_name ScoreResource
 
+signal on_points_changed(value: int)
 
 @export var points: int = 0
 @export var points_per_click: float = 1
@@ -11,12 +13,16 @@ func add_points_from_click():
 
 
 func add_points(value: int):
+	if value == 0:
+		return
 	points += value
+	on_points_changed.emit(points)
 
 
 func spend_points(value: int) -> bool:
 	if value >= points:
 		points -= value
+		on_points_changed.emit(points)
 		return true
 	else:
 		return false
